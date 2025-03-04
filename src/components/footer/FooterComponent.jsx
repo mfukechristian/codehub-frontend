9e5686aa21ab8be0c8168f05544bee0b06aaa03e
mfukechristian <mfukechristian@gmail.com>
2025-03-04 14:42:33 +0200

    add layout components and styles for header, footer, and home page

---
+++ src/components/footer/FooterComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./FooterComponent.css";
+
+const FooterComponent = () => {
+  return (
+    <footer className="footer">
+      <p>Footer</p>
+    </footer>
+  );
+};
+
+export default FooterComponent;
---
+++ src/components/header/HeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HeaderComponent.css";
+
+const HeaderComponent = () => {
+  return (
+    <header className="header">
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default HeaderComponent;
