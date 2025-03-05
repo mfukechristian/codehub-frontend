28ab96122916e22ac157ca4165c7f200cfa54116
mfukechristian <mfukechristian@gmail.com>
2025-03-05 10:28:48 +0200

    update footer component to include current year and author link

--- src/components/footer/FooterComponent.jsx
+++ src/components/footer/FooterComponent.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import "./FooterComponent.css";
 
 const FooterComponent = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p>Footer</p>
+      <p className="footer-text">
+        &copy;{" "}
+        <a
+          href="https://www.linkedin.com/in/christian-mfuke-kambulu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-link"
+        >
+          Christian Mfuke
+        </a>{" "}
+        {currentYear}
+      </p>
     </footer>
   );
 };
